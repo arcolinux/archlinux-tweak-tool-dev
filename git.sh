bf4072d856cbc3b22f57d83ffa08759b462b9f45
Erik Dubois <erik.dubois@gmail.com>
2021-08-18 17:23:21 +0200

    getting code from online

--- git.sh
+++ git.sh
@@ -17,6 +17,27 @@
 echo "Checking for newer files online first"
 git pull
 
+buildFolder=$HOME"/arcolinux-build"
+outFolder=$HOME"/ArcoLinux-Out"
+workdir=$(pwd)
+echo "getting latest .bashrc"
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O $workdir/usr/local/share/arcolinux/.bashrc
+echo "getting latest /etc/sddm.conf"
+wget https://raw.githubusercontent.com/arcolinux/arcolinuxl-iso/master/archiso/airootfs/etc/sddm.conf -O $workdir/usr/local/share/arcolinux/sddm.conf
+FIND="Session=xfce"
+REPLACE="#Session="
+sed -i "s/$FIND/$REPLACE/g" $workdir/usr/local/share/arcolinux/sddm.conf
+
+FIND="User=liveuser"
+REPLACE="#User="
+sed -i "s/$FIND/$REPLACE/g" $workdir/usr/local/share/arcolinux/sddm.conf
+
+
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O $workdir/usr/local/share/arcolinux/arcolinux-mirrorlist
+
+
+
+
 # Below command will backup everything inside the project folder
 git add --all .
 
