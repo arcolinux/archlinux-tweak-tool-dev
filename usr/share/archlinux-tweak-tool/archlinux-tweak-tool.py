0f9193bae534ce0de4457258b6c5257027ebefc4
arcolinuxz <arcolinuxinfo@gmail.com>
2022-05-18 15:17:26 +0200

    catch error

--- usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
+++ usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
@@ -357,17 +357,23 @@ class Main(Gtk.Window):
                 self.sessions.set_sensitive(True)
 
         if os.path.exists("/usr/bin/sddm"):
-            if sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d1),"CursorTheme=") and sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d2),"User="):
-                if Functions.os.path.isfile(Functions.sddm_default_d2):
-                    if "#" in sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d2),"User="):
-                        self.autologin_sddm.set_active(False)
-                        self.sessions_sddm.set_sensitive(False)
-                    else:
-                        self.autologin_sddm.set_active(True)
-                        self.sessions_sddm.set_sensitive(True)
-                if Functions.os.path.isfile(Functions.sddm_default_d1):
-                    read_cursor_name=sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d1),"CursorTheme=").split("=")[1]
-                    self.entry_cursor_name.set_text(read_cursor_name)
+            try:
+                if sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d1),"CursorTheme=") and sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d2),"User="):
+                    if Functions.os.path.isfile(Functions.sddm_default_d2):
+                        if "#" in sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d2),"User="):
+                            self.autologin_sddm.set_active(False)
+                            self.sessions_sddm.set_sensitive(False)
+                        else:
+                            self.autologin_sddm.set_active(True)
+                            self.sessions_sddm.set_sensitive(True)
+                    if Functions.os.path.isfile(Functions.sddm_default_d1):
+                        read_cursor_name=sddm.check_sddm(sddm.get_sddm_lines(Functions.sddm_default_d1),"CursorTheme=").split("=")[1]
+                        self.entry_cursor_name.set_text(read_cursor_name)
+            except Exception as e:
+                print(e)
+                print("Run 'fix-sddm-conf' in a terminal")
+                print("We will make backups of /etc/sddm.conf and /etc/sddm.conf.d/kde_settings.conf")
+                signal_handler()
 
         if not os.path.isfile("/tmp/att.lock"):
             with open("/tmp/att.lock", "w") as f:
