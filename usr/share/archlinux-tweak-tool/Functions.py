b9dca7a1cbf2c899cfbc5055231289d934cd452e
arcolinuxz <arcolinuxinfo@gmail.com>
2022-05-24 20:49:27 +0200

    discovery

--- usr/share/archlinux-tweak-tool/Functions.py
+++ usr/share/archlinux-tweak-tool/Functions.py
@@ -72,6 +72,7 @@ slimlock_conf = "/etc/slim.conf"
 termite_config = home + "/.config/termite/config"
 neofetch_config = home + "/.config/neofetch/config.conf"
 lightdm_conf = "/etc/lightdm/lightdm.conf"
+nsswitch_config ="/etc/nsswitch.conf"
 bd = ".att_backups"
 config = home + "/.config/archlinux-tweak-tool/settings.ini"
 config_dir = home + "/.config/archlinux-tweak-tool/"
@@ -303,6 +304,7 @@ def check_package_installed(package):         # noqa
 # =====================================================
 # =====================================================
 # =====================================================
+
 # =====================================================
 #               ALACRITTY
 # =====================================================
@@ -720,6 +722,19 @@ def close_in_app_notification(self):
     self.timeout_id = None
 
 # =====================================================
+#               NSSWITCH CONF COPY
+# =====================================================
+
+def copy_nsswitch(choice):
+    command ="cp /usr/share/archlinux-tweak-tool/data/" + choice + "/nsswitch.conf /etc/nsswitch.conf"
+    print(command)
+    subprocess.call(command.split(" "),
+                    shell=False,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT)
+    print("/etc/nsswitch.conf has been overwritten - reboot")
+
+# =====================================================
 #               OBLOGOUT CONF
 # =====================================================
 # Get shortcuts index
@@ -833,6 +848,84 @@ def restart_program():
     os.unlink("/tmp/att.lock")
     python = sys.executable
     os.execl(python, python, *sys.argv)
+
+# =====================================================
+#               SERVICES - SAMBA
+# =====================================================
+
+def install_discovery(self):
+    install = 'pacman -S avahi nss-mdns gvfs-smb --needed --noconfirm'
+
+    if check_package_installed("avahi") and check_package_installed("nss-mdns") \
+            and check_package_installed("gvfs-smb"):
+        pass
+    else:
+        subprocess.call(install.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+        print("Avahi, nss-mdns and gvfs-smb is now installed")
+
+    command = 'systemctl enable avahi-daemon.service -f --now'
+    subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    print("We enabled avahi-daemon.service")
+
+def remove_discovery(self):
+
+    command = 'systemctl stop avahi-daemon.service -f --now'
+    subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+
+    command = 'systemctl disable avahi-daemon.service -f --now'
+    subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    print("We disabled avahi-daemon.service")
+
+    command = 'systemctl stop avahi-daemon.socket -f'
+    subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+
+    command = 'systemctl disable avahi-daemon.socket -f'
+    subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    print("We disabled avahi-daemon.socket")
+
+    command = 'pacman -Rs avahi --noconfirm'
+    if check_package_installed("avahi"):
+        subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+        print("Avahi was removed")
+
+    command = 'pacman -Rs nss-mdns --noconfirm'
+    if check_package_installed("nss-mdns"):
+        subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+        print("nss-mdns was removed")
+
+    command = 'pacman -Rs gvfs-smb --noconfirm'
+    if check_package_installed("gvfs-smb"):
+        subprocess.call(command.split(" "),
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+        print("gvfs-smb was removed")
+    else:
+        pass
 
 # =====================================================
 #                       SHELL
--- usr/share/archlinux-tweak-tool/Services_GUI.py
+++ usr/share/archlinux-tweak-tool/Services_GUI.py
@@ -7,7 +7,7 @@ def GUI(self, Gtk, vboxStack14, Functions):
 
     hbox1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
     hbox1_label = Gtk.Label(xalign=0)
-    hbox1_label.set_text("Network")
+    hbox1_label.set_text("Services")
     hbox1_label.set_name("title")
     hbox1.pack_start(hbox1_label, False, False, 10)
 
@@ -15,50 +15,67 @@ def GUI(self, Gtk, vboxStack14, Functions):
     hseparator = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
     hbox0.pack_start(hseparator, True, True, 0)
 
+    vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+    vboxStack1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+    vboxStack2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+    vboxStack3 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+    vboxStack4 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+
+    stack = Gtk.Stack()
+    stack.set_transition_type(Gtk.StackTransitionType.SLIDE_UP_DOWN)
+    stack.set_transition_duration(350)
+    stack.set_hhomogeneous(False)
+    stack.set_vhomogeneous(False)
+
+    stack_switcher = Gtk.StackSwitcher()
+    stack_switcher.set_orientation(Gtk.Orientation.HORIZONTAL)
+    stack_switcher.set_stack(stack)
+    stack_switcher.set_homogeneous(True)
+
+    # ==================================================================
+    #                       SAMBA TAB
+    # ==================================================================
+
     hbox2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
     hbox2_label = Gtk.Label(xalign=0)
     hbox2_label.set_text("Discover other computers in your network")
-    button_Apply_Pacman_Key_Fix = Gtk.Button(label="Install discovery")
-    button_Apply_Pacman_Key_Fix.connect ("clicked", self.on_click_fix_pacman_keys)
-    button_Apply_Pacman_Key_Fixu = Gtk.Button(label="Uninstall discovery")
-    button_Apply_Pacman_Key_Fixu.connect ("clicked", self.on_click_fix_pacman_keys)
+    button_install_discovery = Gtk.Button(label="Install network discovery")
+    button_install_discovery.connect ("clicked", self.on_install_discovery_clicked)
+    button_remove_discovery = Gtk.Button(label="Uninstall network discovery")
+    button_remove_discovery.connect ("clicked", self.on_remove_discovery_clicked)
     hbox2.pack_start(hbox2_label, False, False, 10)
-    hbox2.pack_end(button_Apply_Pacman_Key_Fixu, False, False, 10)
-    hbox2.pack_end(button_Apply_Pacman_Key_Fix, False, False, 10)
+    hbox2.pack_end(button_remove_discovery, False, False, 10)
+    hbox2.pack_end(button_install_discovery, False, False, 10)
 
     hbox3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
     hbox3_label = Gtk.Label(xalign=0)
     hbox3_label.set_text("Change the /etc/nsswitch.conf to connect to computers/NAS")
-    nsswitch_choices = Gtk.ComboBoxText()
+    self.nsswitch_choices = Gtk.ComboBoxText()
     options = ['ArcoLinux', 'Garuda', 'Arch Linux', 'EndeavourOS']
     for option in options:
-        nsswitch_choices.append_text(option)
-    nsswitch_choices.set_active(0)
-    button_Apply_Osbeck = Gtk.Button(label="Apply selected nsswitch")
-    button_Apply_Osbeck.connect ("clicked", self.on_click_fix_mainstream)
-    button_Apply_Osbeckr = Gtk.Button(label="Reset to default nsswitch")
-    button_Apply_Osbeckr.connect ("clicked", self.on_click_fix_mainstream)
+        self.nsswitch_choices.append_text(option)
+    self.nsswitch_choices.set_active(0)
+    button_apply_nsswitch = Gtk.Button(label="Apply selected nsswitch")
+    button_apply_nsswitch.connect ("clicked", self.on_click_apply_nsswitch)
+    button_reset_nsswitch = Gtk.Button(label="Reset to default nsswitch")
+    button_reset_nsswitch.connect ("clicked", self.on_click_reset_nsswitch)
     hbox3.pack_start(hbox3_label, False, False, 10)
 
-    hbox3.pack_end(button_Apply_Osbeckr, False, False, 10)
-    hbox3.pack_end(button_Apply_Osbeck, False, False, 10)
-    hbox3.pack_end(nsswitch_choices, False, False, 10)
+    hbox3.pack_end(button_reset_nsswitch, False, False, 10)
+    hbox3.pack_end(button_apply_nsswitch, False, False, 10)
+    hbox3.pack_end(self.nsswitch_choices, False, False, 10)
 
     hbox4 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
     hbox4_label = Gtk.Label(xalign=0)
     hbox4_label.set_text("Install the samba server - sharing with other computers")
-    button_Apply_Mirrors = Gtk.Button(label="Uninstall Samba")
-    button_Apply_Mirrors.connect ("clicked", self.on_click_get_arch_mirrors)
-    button_Apply_Mirrors2 = Gtk.Button(label="Install Samba")
-    button_Apply_Mirrors2.connect ("clicked", self.on_click_get_arch_mirrors2)
+    button_uninstall_samba = Gtk.Button(label="Uninstall Samba")
+    button_uninstall_samba.connect ("clicked", self.on_click_uninstall_samba)
+    button_install_samba = Gtk.Button(label="Install Samba")
+    button_install_samba.connect ("clicked", self.on_click_install_samba)
     hbox4.pack_start(hbox4_label, False, False, 10)
-    hbox4.pack_end(button_Apply_Mirrors, False, False, 10)
-    hbox4.pack_end(button_Apply_Mirrors2, False, False, 10)
-
-    if not os.path.exists("/usr/bin/reflector"):
-        button_Apply_Mirrors.set_sensitive(False)
-    if not os.path.exists("/usr/bin/rate-mirrors"):
-        button_Apply_Mirrors2.set_sensitive(False)
+    hbox4.pack_end(button_uninstall_samba, False, False, 10)
+    hbox4.pack_end(button_install_samba, False, False, 10)
 
     hbox5 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
     hbox5_label = Gtk.Label(xalign=0)
@@ -102,16 +119,54 @@ def GUI(self, Gtk, vboxStack14, Functions):
     hbox10_label.set_markup("<b>For any Arch Linux based system</b>")
     hbox10.pack_start(hbox10_label, False, False, 10)
 
+
+    vboxStack1.pack_start(hbox2, False, False, 10)
+    vboxStack1.pack_start(hbox3, False, False, 0)
+    vboxStack1.pack_start(hbox4, False, False, 0)
+
+    # ==================================================================
+    #                       SAMBA TAB
+    # ==================================================================
+
+    # ==================================================================
+    #                       PRINTING TAB
+    # ==================================================================
+
+    # ==================================================================
+    #                       BLUETOOTH TAB
+    # ==================================================================
+
+    # ==================================================================
+    #                       AUDIO TAB
+    # ==================================================================
+
+    # ==================================================================
+    #                       PACK TO STACK
+    # ==================================================================
+
+    stack.add_titled(vboxStack1, "stack1", "Samba")
+    stack.add_titled(vboxStack2, "stack2", "Printing")
+    stack.add_titled(vboxStack3, "stack3", "Bluetooth")
+    stack.add_titled(vboxStack4, "stack4", "Audio")
+
+    vbox.pack_start(stack_switcher, False, False, 0)
+    vbox.pack_start(stack, True, True, 0)
+
+    vboxStack14.pack_start(hbox1, False, False, 0)
+    vboxStack14.pack_start(hbox0, False, False, 0)
+    vboxStack14.pack_start(vbox, True, True, 0)
+
     # ======================================================================
     #                       VBOX STACK
     # ======================================================================
 
-    vboxStack14.pack_start(hbox1, False, False, 0)
-    vboxStack14.pack_start(hbox0, False, False, 0)
-    vboxStack14.pack_start(hbox10, False, False, 20) # title for any arch linux based system
-    vboxStack14.pack_start(hbox2, False, False, 0)
-    vboxStack14.pack_start(hbox3, False, False, 0)
-    vboxStack14.pack_start(hbox4, False, False, 0)
+    # vboxStack14.pack_start(hbox1, False, False, 0)
+    # vboxStack14.pack_start(hbox0, False, False, 0)
+    # vboxStack14.pack_start(hbox10, False, False, 20) # title for any arch linux based system
+    # vboxStack14.pack_start(hbox2, False, False, 0)
+    # vboxStack14.pack_start(hbox3, False, False, 0)
+    # vboxStack14.pack_start(hbox4, False, False, 0)
+
     #vboxStack14.pack_start(hbox7, False, False, 0)
     #vboxStack14.pack_start(hbox8, False, False, 0)
     #vboxStack14.pack_start(hbox9, False, False, 20)
