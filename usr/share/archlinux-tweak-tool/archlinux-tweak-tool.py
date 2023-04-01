eeab0a029a0c805006f3f220207c22a213385528
Erik Dubois <erik.dubois@gmail.com>
2023-04-01 05:50:50 +0200

    wayland fix

--- usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
+++ usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
@@ -4564,13 +4564,12 @@ if __name__ == "__main__":
         fn.getlogin()
     except:
         os_function_support = False
-    if not fn.path.isfile("/tmp/att.lock") and os_function_support
+    if not fn.path.isfile("/tmp/att.lock") and os_function_support:
         with open("/tmp/att.pid", "w", encoding="utf-8") as f:
             f.write(str(fn.getpid()))
             f.close()
         style_provider = Gtk.CssProvider()
         style_provider.load_from_path(base_dir + "/att.css")
-
 
         Gtk.StyleContext.add_provider_for_screen(
             Gdk.Screen.get_default(),
