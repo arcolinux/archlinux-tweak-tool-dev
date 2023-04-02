4c43f58d1ee16936d2cd62cc229d0598598b9519
Erik Dubois <erik.dubois@gmail.com>
2023-04-02 20:47:47 +0200

    no services

--- usr/share/archlinux-tweak-tool/gui.py
+++ usr/share/archlinux-tweak-tool/gui.py
@@ -288,7 +288,8 @@ themes</i> you can customize <b>Neofetch</b>"
 
     stack.add_titled(vboxstack3, "stack2", "Privacy")  # Privacy
 
-    stack.add_titled(vboxstack14, "stack14", "Services")  # services
+    if fn.distr != "artix":
+        stack.add_titled(vboxstack14, "stack14", "Services")  # services
 
     stack.add_titled(vboxstack23, "stack23", "Shells")  # shell
 
